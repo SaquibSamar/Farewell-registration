b2fd7d1d49b974203876f16e8ffe30bdc5567a23
SaquibSamar <samarsaquib15@gmail.com>
2026-05-10 00:21:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,78 @@ document.getElementById("farewellForm");
 
 form.addEventListener(
   "submit",
-  function(e){
+  async function(e){
 
     e.preventDefault();
 
+    const inputs =
+    form.querySelectorAll("input");
+
+    const name = inputs[0].value;
+    const phone = inputs[1].value;
+    const course = inputs[2].value;
+
+    const file =
+    inputs[3].files[0];
+
+    // Loading
+    const button =
+    form.querySelector("button");
+
+    button.innerText =
+    "Uploading...";
+
+    // Upload To Cloudinary
+    const data =
+    new FormData();
+
+    data.append(
+      "file",
+      file
+    );
+
+    data.append(
+      "upload_preset",
+      "farewell_upload"
+    );
+
+    const cloudinaryResponse =
+    await fetch(
+      "https://api.cloudinary.com/v1_1/dsbougenq/image/upload",
+      {
+        method:"POST",
+        body:data
+      }
+    );
+
+    const cloudinaryData =
+    await cloudinaryResponse.json();
+
+    // Send To Google Sheet
+    await fetch(
+      "https://script.google.com/macros/s/AKfycbzsZ0xsBjPrcfpzE-QjEX3jwY7GZVFdhCm8pBBrrMQZb4zXGCg8KrNMdLPPWha1lqK5zQ/exec",
+      {
+        method:"POST",
+
+        body:JSON.stringify({
+          name:name,
+          phone:phone,
+          course:course,
+          screenshot:
+          cloudinaryData.secure_url
+        })
+      }
+    );
+
+    button.innerText =
+    "Confirm Registration";
+
     alert(
-      "Registration Submitted 🎉"
+      "Registration Successful 🎉"
     );
 
     form.reset();
+
   }
 );
 
@@ -26,7 +89,7 @@ function scrollToForm(){
 
 }
 
-// Scroll Reveal Animation
+// Scroll Reveal
 const hiddenElements =
 document.querySelectorAll(".hidden");
 
@@ -39,7 +102,10 @@ window.addEventListener(
       const elementTop =
       el.getBoundingClientRect().top;
 
-      if(elementTop < window.innerHeight - 100){
+      if(
+        elementTop <
+        window.innerHeight - 100
+      ){
         el.classList.add("show");
       }
 
